6d53853d89368986a4caee580f5a0c59eb584d1b
Sofiane <soufiane.belghit@gmail.com>
2019-02-26 14:28:03 +0100

    push final

---
+++ resources/views/kanpai.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <title>Ecommerce projet</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+  
+<main>
+
+  <div class="enorme col-lg-12 col-md-12 col-12">
+    <H1>EN COURS</H1>
+  </div>
+
+  
+         
+
+  <div class="enorme col-lg-12 col-md-12 col-12">
+    <H1>A VENIR</H1>
+  </div>
+  
+
+
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+  <div class="fond">
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row py-4 d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+          <h6 class="mb-0">RESTEZ CONNECTE AVEC NOUS SUR LES RESEAUX SOCIAUX !</h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+          <!-- Facebook -->
+          <a class="ic">
+            <img src="facebook.png">
+          </a>
+          <!-- Twitter -->
+          <a class="ic">
+            <img src="twitter.png">
+          </a>
+          <!-- Google +-->
+          <a class="ic">
+            <img src="pinterest.png">
+          </a>
+          <!--Linkedin -->
+          <a class="ic">
+            <img src="deviantart.png">
+          </a>
+          <!--Instagram-->
+          <a class="ic">
+            <img src="flickr.png">
+          </a>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
+    </div>
+  </div>
+
+  <!-- Footer Links -->
+  <div class="container fond2 text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">Sitoupouri</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p style="justify-content: center;">À travers notre plateforme, nous vous donnons accès au plus vaste catalogue d’oeuvres <strong>Sitoupouri</strong> rassemble des oeuvres d’artistes mondialement reconnus et de jeunes talents.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Catégories</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">Campagne en cours</a>
+        </p>
+        <p>
+          <a href="#!">Campagne à venir</a>
+        </p>
+        <p>
+          <a href="#!">Toutes les campagnes</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Mon compte</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">Mes commandes</a>
+        </p>
+        <p>
+          <a href="#!">Mes informations</a>
+        </p>
+        <p>
+          <a href="#!">Mes réductions</a>
+        </p>
+        <p>
+          <a href="#!">Mes adresses</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fa fa-home mr-3"></i>Saint-Etienne 42, Telecom, FR</p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i> Sitoupouri@entreprise.com</p>
+            <p>
+              <i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+              <p>
+                <i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+
+              </div>
+              <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+          </div>
+          <!-- Footer Links -->
+
+          <!-- Copyright -->
+          <div class="footer-copyright text-center py-3">© 2019 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/"> Design Tech Academy</a>
+          </div>
+          <!-- Copyright -->
+
+        </footer>
+
+        <div class="container logo-pied">
+          <div class="row">
+            <img src="logo.png">
+          </div>
+        </div>
+        <!-- Footer -->
+
+
+      </div>
+
+    </body>
+
+    <script type="text/javascript" src="main.js"></script>
+
+    </html>
